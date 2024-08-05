54c95a0419d2d8f0081d2b0fbf62ae57593aa41e
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-08-05 18:51:15 +0800

    change variable name

--- src/api/getRecipeData.ts
+++ src/api/getRecipeData.ts
@@ -1,11 +1,11 @@
 import axios, { AxiosResponse } from "axios";
 
 const BASE_URL = 'https://api.spoonacular.com';
-const API_URL = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 export async function getRandomRecipe() {
   try {
-    const response = await axios.get(`${BASE_URL}/recipes/random?apiKey=${API_URL}`)
+    const response = await axios.get(`${BASE_URL}/recipes/random?apiKey=${API_KEY}`)
     return response;
   } catch (error) {
     console.log(error);
@@ -14,7 +14,7 @@ export async function getRandomRecipe() {
 
 const getFeaturedRecipesId = async() => {
   try {
-    const response: AxiosResponse = await axios.get(`${BASE_URL}/recipes/complexSearch?apiKey=${API_URL}&instructionsRequired=true&fillIngredients=false&addRecipeInformation=false&addRecipeNutrition=false&maxReadyTime=20&ignorePantry=false&sort=meta-score&sortDirection=desc&number=2&limitLicense=true`)
+    const response: AxiosResponse = await axios.get(`${BASE_URL}/recipes/complexSearch?apiKey=${API_KEY}&instructionsRequired=true&fillIngredients=false&addRecipeInformation=false&addRecipeNutrition=false&maxReadyTime=20&ignorePantry=false&sort=meta-score&sortDirection=desc&number=2&limitLicense=true`)
     return response.data.results;
   } catch (error) {
     console.log(error);
@@ -26,7 +26,7 @@ export const getFeaturedRecipesInformation = async () => {
   const recipeId = response.map((data: {id: number}) => data.id)
 
   try {
-    const response: AxiosResponse = await axios.get(`${BASE_URL}/recipes//informationBulk?apiKey=${API_URL}&ids=${recipeId}&includeNutrition=true`)
+    const response: AxiosResponse = await axios.get(`${BASE_URL}/recipes//informationBulk?apiKey=${API_KEY}&ids=${recipeId}&includeNutrition=true`)
     return response.data;
   } catch (error) {
     console.log(error);
