26f76cf78f5ee7be89c2d1482b3d400c372c6aaf
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-08-14 18:22:05 +0800

    style: add links and improve design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,11 +3,27 @@ import logo from '/logo.svg';
 
 export const Footer = () => {
   return (
-    <footer className="bg-black-950 flex flex-col px-4 py-8 gap-6 items-center">
-      <img src={logo} alt="" />
+    <footer className="bg-black-950 flex flex-row px-4 py-4 gap-4 items-start justify-between">
+      <div className="h-full">
+        <img src={logo} alt="" />
+      </div>
 
-      <div className="flex flex-col gap-2 items-center">
-        <Link to={'/explore'}>Explore</Link>
+      <div className="flex flex-col gap-4">
+        <p className="">Lapoar</p>
+
+        <div className="flex flex-col gap-4">
+          <Link to={'/'}>Home</Link>
+          <Link to={'/explore'}>Explore</Link>
+          <Link to={'/collection'}>Collection</Link>
+        </div>
+      </div>
+
+      <div className="flex flex-col gap-4">
+        <p className="">API</p>
+
+        <div className="flex flex-col gap-4">
+          <a href="https://spoonacular.com/food-api">spoonacular</a>
+        </div>
       </div>
     </footer>
   );
