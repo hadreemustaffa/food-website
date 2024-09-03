d353b178a16176542a4340b76044b420a1d0515a
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-09-03 14:24:21 +0800

    refactor: change router type, add error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,12 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Link,
+  Navigate,
+  Route,
+  RouterProvider,
+  useRouteError,
+} from 'react-router-dom';
 
 import { Home } from './pages/Home';
 import { Explore } from './pages/Explore';
@@ -8,24 +16,46 @@ import { Layout } from './components/Layout';
 import { Collection } from './pages/Collection';
 import { Search } from './pages/Search';
 
-function App() {
+export function ErrorBoundary() {
+  let error = useRouteError();
+  console.error(error);
+
   return (
-    <>
-      <BrowserRouter>
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path='/' element={<Home />} />
-            <Route path='/explore' element={<Explore />} />
-            <Route path='/recipe/:recipeId' element={<Recipe />} />
-            <Route path='/collection' element={<Collection />} />
-            <Route path='/search/:query' element={<Search />} />
-            <Route path='/404' element={<NotFound />} />
-            <Route path='*' element={<Navigate to='/404' replace />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </>
+    <div className='col-span-full m-auto flex w-fit flex-col gap-4 text-left'>
+      <h1>Oops! There seem to be an error while fetching the data!</h1>
+      <Link to='/'>Back to Homepage</Link>
+    </div>
   );
+}
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout />}>
+      <Route path='/' element={<Home />} errorElement={<ErrorBoundary />} />
+      <Route
+        path='/explore'
+        element={<Explore />}
+        errorElement={<ErrorBoundary />}
+      />
+      <Route
+        path='/recipe/:recipeId'
+        element={<Recipe />}
+        errorElement={<ErrorBoundary />}
+      />
+      <Route path='/collection' element={<Collection />} />
+      <Route
+        path='/search?q=:query'
+        element={<Search />}
+        errorElement={<ErrorBoundary />}
+      />
+      <Route path='/404' element={<NotFound />} />
+      <Route path='*' element={<Navigate to='/404' replace />} />
+    </Route>
+  )
+);
+
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
