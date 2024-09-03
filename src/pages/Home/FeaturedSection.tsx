d192444f731c6382107824eca5c1708d32bf6f0a
hadreemustaffa <hadree.mustaffa@gmail.com>
2024-09-03 14:20:38 +0800

    refactor: remove state and effect hook, save to local

--- src/pages/Home/FeaturedSection.tsx
+++ src/pages/Home/FeaturedSection.tsx
@@ -1,39 +1,39 @@
-import { lazy, Suspense } from 'react';
 import { getFeaturedSectionRecipesId } from '../../api/getRecipeData';
 
 import { RecipeCardMinimal } from '../../components/RecipeCard';
 import { RecipeMinimalProps } from '../../interfaces';
 import { CardListLoader } from '../../components/Skeleton';
-
-const CardList = lazy(() =>
-  getFeaturedSectionRecipesId().then((data) => {
-    return {
-      default: () => {
-        const listItems = data.map((recipe: RecipeMinimalProps) => (
-          <li key={recipe.id}>
-            <RecipeCardMinimal
-              id={recipe.id}
-              name={recipe.title}
-              imagePath={recipe.image}
-            />
-          </li>
-        ));
-
-        return (
-          <ul className='grid grid-cols-auto-fill-225 gap-4'>{listItems}</ul>
-        );
-      },
-    };
-  })
-);
+import useFetchRecipeList from '../../hooks/useFetchRecipeList';
 
 export const FeaturedSection = () => {
+  const recipes = useFetchRecipeList({
+    storageKey: 'featured-recipes',
+    request: async () => {
+      const response = await getFeaturedSectionRecipesId();
+      return response;
+    },
+  });
+
   return (
     <div className='col-span-1 row-start-3 flex flex-col gap-4 sm:col-span-2 md:col-span-3 lg:col-span-4'>
       <h2 className='font-sans text-4xl font-bold'>Featured</h2>
-      <Suspense fallback={<CardListLoader itemCount={4} />}>
-        <CardList />
-      </Suspense>
+      {recipes ? (
+        <>
+          <ul className='grid grid-cols-auto-fill-225 gap-4'>
+            {recipes.map((recipe: RecipeMinimalProps) => (
+              <li key={recipe.id}>
+                <RecipeCardMinimal
+                  id={recipe.id}
+                  name={recipe.title}
+                  imagePath={recipe.image}
+                />
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <CardListLoader itemCount={4} />
+      )}
     </div>
   );
 };
--- src/pages/Home/PopularSection.tsx
+++ src/pages/Home/PopularSection.tsx
@@ -1,39 +1,39 @@
-import { lazy, Suspense } from 'react';
 import { getPopularSectionRecipesId } from '../../api/getRecipeData';
 
 import { RecipeCardMinimal } from '../../components/RecipeCard';
-import { RecipeMinimalProps } from '../../interfaces';
 import { CardListLoader } from '../../components/Skeleton';
-
-const CardList = lazy(() =>
-  getPopularSectionRecipesId().then((data) => {
-    return {
-      default: () => {
-        const listItems = data.map((recipe: RecipeMinimalProps) => (
-          <li key={recipe.id}>
-            <RecipeCardMinimal
-              id={recipe.id}
-              name={recipe.title}
-              imagePath={recipe.image}
-            />
-          </li>
-        ));
-
-        return (
-          <ul className='grid grid-cols-auto-fill-225 gap-4'>{listItems}</ul>
-        );
-      },
-    };
-  })
-);
+import useFetchRecipeList from '../../hooks/useFetchRecipeList';
 
 export const PopularSection = () => {
+  const recipes = useFetchRecipeList({
+    storageKey: 'popular-recipes',
+    request: async () => {
+      const response = await getPopularSectionRecipesId();
+      return response;
+    },
+  });
+
   return (
     <div className='col-span-1 row-start-4 flex flex-col gap-4 sm:col-span-2 md:col-span-3 lg:col-span-4'>
       <h2 className='font-sans text-4xl font-bold'>Popular</h2>
-      <Suspense fallback={<CardListLoader itemCount={4} />}>
-        <CardList />
-      </Suspense>
+
+      {recipes ? (
+        <>
+          <ul className='grid grid-cols-auto-fill-225 gap-4'>
+            {recipes.map((recipe) => (
+              <li key={recipe.id}>
+                <RecipeCardMinimal
+                  id={recipe.id}
+                  name={recipe.title}
+                  imagePath={recipe.image}
+                />
+              </li>
+            ))}
+          </ul>
+        </>
+      ) : (
+        <CardListLoader itemCount={4} />
+      )}
     </div>
   );
 };
